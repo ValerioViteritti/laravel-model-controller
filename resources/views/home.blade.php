52c2c20b1d80326a204c84a2c46daeb820bacaad
ValerioViteritti <vviteritti@gmail.com>
2024-09-11 16:40:05 +0200

    feat: card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,25 +8,35 @@
 @section('content')
     @dump($movies)
     <div class="container my-5">
-        <h1></h1>
-        {{-- <img src="{{ Vite::asset('resources/img/colibri.jpg') }}" alt="" class="img-fluid"> --}}
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                    content.</p>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
-            </ul>
-            <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-            </div>
+        <div class="row">
+
+            <h1></h1>
+            {{-- <img src="{{ Vite::asset('resources/img/colibri.jpg') }}" alt="" class="img-fluid"> --}}
+
+            @foreach ($movies as $movie)
+                <div class="card" style="width: 15rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <p class="card-text">{{ $movie->description }}</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Titolo originale: {{ $movie->original_title }}</li>
+                        <li class="list-group-item">Nazionalità: {{ $movie->nationality }}</li>
+                        <li class="list-group-item">Data: {{ $movie->date }}</li>
+
+                    </ul>
+                    <div class="card-body">
+                        {{-- <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a> --}}
+                    </div>
+                </div>
+            @endforeach
+
+
+
         </div>
+
+
     </div>
 @endsection
 
