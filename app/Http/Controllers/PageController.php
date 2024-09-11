c1425ff47b6a2b96ed14a21026414ac8200eeab2
ValerioViteritti <vviteritti@gmail.com>
2024-09-11 16:07:50 +0200

    feat: initialized the project, add library, add routes

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Movie;
+
+class PageController extends Controller
+{
+
+    public function index(){
+
+        $movies = Movie::all();
+
+        return view('home', compact('movies'));
+
+    }
+
+    public function about(){
+
+        return view('about');
+
+    }
+
+    public function contacts(){
+
+        return view('contacts');
+
+    }
+
+    
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,14 +6,28 @@
 @endsection
 
 @section('content')
-<div class="container my-5">
-    <h1>{{ $title }}</h1>
-    <img src="{{ Vite::asset('resources/img/colibri.jpg') }}" alt="" class="img-fluid">
-    <p>
-        {{ $text }}
-    </p>
-</div>
-
+    @dump($movies)
+    <div class="container my-5">
+        <h1></h1>
+        {{-- <img src="{{ Vite::asset('resources/img/colibri.jpg') }}" alt="" class="img-fluid"> --}}
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                    content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">An item</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+            </ul>
+            <div class="card-body">
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+    </div>
 @endsection
 
 
