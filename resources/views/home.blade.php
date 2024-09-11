6766631d6b53cf7040a096258f0eec269fd05579
ValerioViteritti <vviteritti@gmail.com>
2024-09-11 17:06:33 +0200

    feat: add img and description; fix: order card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
 
             @foreach ($movies as $movie)
                 <div class="card" style="width: 15rem;">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="{{ $movie->img }}" class="card-img-top" alt="{{ $movie->title }}">
                     <div class="card-body">
                         <h5 class="card-title">{{ $movie->title }}</h5>
                         <p class="card-text">{{ $movie->description }}</p>
